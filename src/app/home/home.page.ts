74fbebf5860f0f0eb8ea674858969d3e0d22c295
ANBASS <anbass@email.sc.edu>
2019-05-03 12:06:11 -0400

    Choose Life

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,9 +10,26 @@ export class HomePage {
     Life1: number;
     Life2: number;
 
-    constructor() {
+    constructor(public alertCtrl: AlertController) {
         this.Life1 = 20;
         this.Life2 = 20;
+    }
+
+    ionViewDidLoad() {
+        this.startingLife();
+    }
+
+    startingLife() {
+        let alert = this.alertCtrl.create({
+            message: 'Choose Your Starting Life!',
+            inputs: [{
+                name: 'Life',
+                placeholder: 'Starting Life'
+            },],
+            buttons: [{ text: 'Okay', handler: (data) => this.setLife(data.Life)}]
+
+        });
+        alert.present();
     }
 
     AddNumber1() {
@@ -28,8 +46,7 @@ export class HomePage {
         this.Life2--;
     }
 
-
-    Reset() {
+    set20() {
         this.Life1 = 20;
         this.Life2 = 20;
     }
@@ -42,5 +59,10 @@ export class HomePage {
     set40() {
         this.Life1 = 40;
         this.Life2 = 40;
+    }
+
+    setLife(a) {
+        this.Life1 = a;
+        this.Life2 = a;
     }
 }
